6c0da690c414e17f2389058c5ec20241e792feee
Tharindi-mi <uvini.tharindi.11@gmail.com>
2023-12-06 20:56:17 +0530

    Main- service

--- commit-service/index.js
+++ commit-service/index.js
@@ -1,21 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = process.env.PORT | 3000;
-
-app.get('/', (req, res) => {
-    res.json({health:"Commits Service UP"})
-})
-
-app.get('/api/commits/:username/:repository', (req, res) => {
-
-  axios.get(`https://api.github.com/repos/${req.params.username}/${req.params.repository}/pulls`).then((response)=>{
-    res.json(response.data)
-  }).catch((e)=>{
-    res.json({message:e.message})
-  })
-})
+const app = require('./app');
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Commits service app listening`)
+  console.log(`Commit service app listening on port ${port}`)
 })
