628a9610257c97883f2bf69372e4872201e655a6
Tharindi-mi <uvini.tharindi.11@gmail.com>
2023-12-07 17:34:24 +0530

    update test

--- main-service/app.js
+++ main-service/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-const { pullrequestservice } = require('./pullrequestservice');
 const app = express();
 
 app.use(express.json());
